a6fa21abc5431557cea6c97e3cadf805077e286a
coderman0 <81552900+coderman0@users.noreply.github.com>
2022-03-10 20:45:32 +0300

    Add files via upload

---
+++ lib/app/modules/home/binding.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:getx_todo_list/app/data/providers/task/provider.dart';
+import 'package:getx_todo_list/app/data/services/storage/repository.dart';
+
+import 'controller.dart';
+
+class HomeBinding implements Bindings {
+  @override
+  void dependencies() {
+   Get.lazyPut(() => HomeController(
+     taskRepository: TaskRepository(
+       taskProvider: TaskProvider(),
+     ),
+   ),
+   );
+  }
+
+}
